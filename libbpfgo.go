8432b358e39011171adf2a5aba59c08866abd71d
Jeff Mahoney <jeffm@suse.com>
2023-03-08 15:45:57 -0500

    Fix full-static mode builds.
    Commit 81f68f851c0 started setting LDFLAGS for static mode builds
    but didn't include the libbpf.a archive.

--- libbpfgo.go
+++ libbpfgo.go
@@ -1,7 +1,7 @@
 package libbpfgo
 
 /*
-#cgo !libbpfgo_full_static LDFLAGS: -lelf -lz
+#cgo  libbpfgo_static LDFLAGS: ${SRCDIR}/output/libbpf.a -lelf -lz
 
 #cgo  libbpfgo_full_static CFLAGS: -I${SRCDIR}/output -I${SRCDIR}/include/uapi
 #cgo  libbpfgo_full_static LDFLAGS: ${SRCDIR}/output/libbpf.a -l:libelf.a -l:libz.a
